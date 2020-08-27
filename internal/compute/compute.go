99b722813b98a48d119abfc45c876e817d7502c3
nokamoto <nokamoto.engr@gmail.com>
2020-08-27 21:08:45 +0900

    Implement service compute.create

--- internal/compute/compute.go
+++ internal/compute/compute.go
@@ -3,6 +3,7 @@ package compute
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 	"strings"
 
 	"github.com/jinzhu/gorm"
@@ -17,6 +18,18 @@ type Compute struct {
 	instanceQuery instanceQuery
 	projectQuery  projectQuery
 	db            *gorm.DB
+}
+
+// RandomName returns a genrated name randomly from the parent id.
+func (c *Compute) RandomName(parentID string) string {
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz0123456789")
+
+	b := make([]rune, 8)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+
+	return fmt.Sprintf("%s-%s", parentID, string(b))
 }
 
 // Create creates a new compute instance.
